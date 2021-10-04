86e41667a7cb19c795108afc5c6be9e42cb0132f
DoHuyToan <dhtoan89@gmail.com>
2021-10-04 07:26:49 +0700

    chua xong

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        HotelManager hotelToanDo = new HotelManager();
+        ArrayList<Customer> customerArrayList = new ArrayList<>();
+
+    }
+}
